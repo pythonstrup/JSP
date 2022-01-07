464074564738b10b610c18f1b176f7be694de07b
BigBell <90585081+pythonstrup@users.noreply.github.com>
2022-01-07 11:31:21 +0900

    마무리(Delete 기능 추가)
    CRUD 기능을 가진 학생 관리 어플리케이션

--- web-student-tracker/src/main/java/com/bigbell/web/jdbc/StudentControllerServlet.java
+++ web-student-tracker/src/main/java/com/bigbell/web/jdbc/StudentControllerServlet.java
@@ -68,12 +68,29 @@ public class StudentControllerServlet extends HttpServlet {
 				updateStudent(request, response);
 				break;
 				
+			case "DELETE":
+				deleteStudent(request, response);
+				break;
+				
 			default:
 				listStudents(request, response);
 			}
 		} catch (Exception exc) {
 			throw new ServletException(exc);
 		}
+	}
+
+	private void deleteStudent(HttpServletRequest request, HttpServletResponse response) 
+		throws Exception {
+		
+		// read student id from form data
+		String theStudentId = request.getParameter("studentId");
+		
+		// delete student from database
+		studentDbUtil.deleteStudent(theStudentId);
+		
+		// send them back to "list students" page
+		listStudents(request, response);
 	}
 
 	private void updateStudent(HttpServletRequest request, HttpServletResponse response) 
