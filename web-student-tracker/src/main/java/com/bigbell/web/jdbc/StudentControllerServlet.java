38dd707724e856a358e990e3723969550e09e8e6
BigBell <90585081+pythonstrup@users.noreply.github.com>
2022-01-07 11:07:56 +0900

    Update 기능 추가

--- web-student-tracker/src/main/java/com/bigbell/web/jdbc/StudentControllerServlet.java
+++ web-student-tracker/src/main/java/com/bigbell/web/jdbc/StudentControllerServlet.java
@@ -60,12 +60,58 @@ public class StudentControllerServlet extends HttpServlet {
 				addStudent(request, response);
 				break;
 				
+			case "LOAD":
+				loadStudent(request, response);
+				break;
+				
+			case "UPDATE":
+				updateStudent(request, response);
+				break;
+				
 			default:
 				listStudents(request, response);
 			}
 		} catch (Exception exc) {
 			throw new ServletException(exc);
 		}
+	}
+
+	private void updateStudent(HttpServletRequest request, HttpServletResponse response) 
+		throws Exception {
+		
+		// read student info from form data
+		int id =  Integer.parseInt(request.getParameter("studentId"));
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		
+		// create a new student object
+		Student theStudent = new Student(id, firstName, lastName, email);
+		
+		// perform update on database
+		studentDbUtil.updateStudent(theStudent);
+		
+		// send them back to the "list students" page
+		listStudents(request, response);
+	}
+
+	private void loadStudent(HttpServletRequest request, HttpServletResponse response) 
+		throws Exception {
+		
+		// read student id from form data
+		String theStudentId = request.getParameter("studentId");
+		
+		// get student from database (db util)
+		Student theStudent = studentDbUtil.getStudent(theStudentId);
+		
+		// place student in the request attribute
+		request.setAttribute("THE_STUDENT", theStudent);
+		
+		// send to jsp page: update-student-form.jsp
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher("/update-student-form.jsp");
+		dispatcher.forward(request, response);
+		
 	}
 
 	private void addStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
