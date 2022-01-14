195382e7e8e4c290de331d52c136ca354a4c96c5
BigBell <90585081+pythonstrup@users.noreply.github.com>
2022-01-14 18:40:16 +0900

    회원제 게시판 만들기1
    CRUD 중 쓰기, 읽기, 수정하기까지 완료

---
+++ MustHaveJSP/memberSQL/더미데이터추가.sql
@@ -0,0 +1,5 @@
+INSERT INTO board(title, content, id, postdate, visitcount)
+VALUES("지금은 봄입니다", "봄의 왈츠", "musthave", NOW(), 0),
+('지금은 여름입니다', '여름향기', 'musthave', NOW(), 0),
+('지금은 가을입니다', '가을동화', 'musthave', NOW(), 0),
+('지금은 겨울입니다', '겨울연가', 'musthave', NOW(), 0);
