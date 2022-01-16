e0ee99bcb308849435275d9447d29e48087137a9
BigBell <90585081+pythonstrup@users.noreply.github.com>
2022-01-16 16:54:47 +0900

    페이징기능을 추가한 게시판(MySQL 버전)
    MySQL로 새롭게 구성한 페이징 게시판

--- MustHaveJSP/memberSQL/더미데이터추가.sql
+++ MustHaveJSP/memberSQL/더미데이터추가.sql
@@ -3,3 +3,52 @@ VALUES("지금은 봄입니다", "봄의 왈츠", "musthave", NOW(), 0),
 ('지금은 여름입니다', '여름향기', 'musthave', NOW(), 0),
 ('지금은 가을입니다', '가을동화', 'musthave', NOW(), 0),
 ('지금은 겨울입니다', '겨울연가', 'musthave', NOW(), 0);
+
+INSERT INTO board(title, content, id, postdate, visitcount)
+VALUES("페이징 처리", "봄의 왈츠", "musthave", NOW(), 0),
+('페이징 처리', '여름향기', 'musthave', NOW(), 0),
+('페이징 처리', '가을동화', 'musthave', NOW(), 0),
+('페이징 처리', '겨울연가', 'musthave', NOW(), 0);
+
+
+# rownum 사용법 _ 찾아서 공부해본 것
+SET @ROWNUM:=0;
+
+SELECT
+	id
+	,pass
+    ,@ROWNUM:=@ROWNUM+1 AS ROWNUM
+FROM member, (SELECT @ROWNUM:=0) R;
+    
+    
+SELECT
+	@ROWNUM:=@ROWNUM+1 AS ROWNUM
+	, id
+    , pass
+FROM member
+WHERE (SELECT @ROWNUM:=0)=0;
+    
+    
+    
+# 페이징 처리용 쿼리문 작성
+SELECT * FROM board 
+WHERE title LIKE "%페이징%" ORDER BY num DESC
+LIMIT 10, 20;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- MustHaveJSP/src/main/java/model1/board/BoardDAO.java
+++ MustHaveJSP/src/main/java/model1/board/BoardDAO.java
@@ -72,6 +72,48 @@ public class BoardDAO extends JDBConnect{
 		return bbs;
 	}
 	
+	public List<BoardDTO> selectListPage(Map<String, Object> map) {
+		List<BoardDTO> bbs = new Vector<BoardDTO>();
+		
+		String query = "SELECT * FROM board ";
+					 
+		
+		if (map.get("searchWord") != null) {
+			query += " WHERE " + map.get("searchField")
+				  + " LIKE '%" + map.get("searchWord") + "%' ";
+		}
+		
+		query += " ORDER BY num DESC "
+			  + "LIMIT ?, 10";
+		
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setInt(1, Integer.parseInt(map.get("start").toString()));
+//			psmt.setString(2, map.get("end").toString());
+			
+			rs = psmt.executeQuery();
+			
+			while (rs.next()) {
+				BoardDTO dto = new BoardDTO();
+				dto.setNum(rs.getString("num"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setPostdate(rs.getDate("postdate"));
+				dto.setId(rs.getString("id"));
+				dto.setVisitcount(rs.getString("visitcount"));
+				
+				bbs.add(dto);
+			}
+			
+		} catch(Exception e) {
+			System.out.println("게시물 조회 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return bbs;
+	}
+	
+	
 	public int insertWriter(BoardDTO dto) {
 		int result = 0;
 		
@@ -132,7 +174,7 @@ public class BoardDAO extends JDBConnect{
 		try {
 			psmt = con.prepareStatement(query);
 			psmt.setString(1, num);
-			psmt.executeQuery();
+			psmt.executeUpdate();
 		} catch(Exception e) {
 			System.out.println("게시물 조회수 증가 중 예외 발생");
 			e.printStackTrace();
@@ -160,6 +202,26 @@ public class BoardDAO extends JDBConnect{
 		
 		return result;
 	}
+	
+	public int deletePost(BoardDTO dto) {
+		int result = 0;
+		
+		try {
+			String query = "DELETE FROM board WHERE num=?";
+			
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getNum());
+			
+			result = psmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("게시물 삭제 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	
 }
 
 
